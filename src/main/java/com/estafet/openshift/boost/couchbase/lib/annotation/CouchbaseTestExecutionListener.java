1736b7d82e3858746ce548b799b28347bcdbf9c1
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-04-24 20:35:57 +0100

    Added basic setup

---
+++ src/main/java/com/estafet/openshift/boost/couchbase/lib/annotation/CouchbaseTestExecutionListener.java
@@ -0,0 +1,16 @@
+package com.estafet.openshift.boost.couchbase.lib.annotation;
+
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+
+public class CouchbaseTestExecutionListener extends AbstractTestExecutionListener {
+
+	@Override
+	public void beforeTestMethod(TestContext testContext) throws Exception {
+		// TODO Auto-generated method stub
+		super.beforeTestMethod(testContext);
+	}
+
+	
+	
+}
